4a8eaa2b62109f2c4d20cbbcfadcf012e25543f9
Matt Farmer <mrfarmer777@gmail.com>
2018-05-28 01:30:45 +0000

    automatically backed up by learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -3,3 +3,24 @@ def sort_array_asc(array)
   sorted=array.sort
   return sorted
 end
+
+
+def sort_array_desc(array)
+  sorted=array.sorted
+  sorted.reverse!
+  return sorted
+end
+
+def swap_elements(array)
+  temp=array[2]
+  array[2]=array[3]
+  array[3]=temp
+  return array
+end
+
+def swap_elements_from_to(array,from_ind,to_ind)
+  temp=array[from_ind]
+  array[from_ind]=array[to_ind]
+  array[to_ind]=temp
+  return array
+end
