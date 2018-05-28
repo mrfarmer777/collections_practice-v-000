f21e59a21d842fa71b794f970d5d97d58881ec8e
Matt Farmer <mrfarmer777@gmail.com>
2018-05-28 01:46:33 +0000

    automatically backed up by learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -11,6 +11,11 @@ def sort_array_desc(array)
   return sorted
 end
 
+def sort_array_char_count(array)
+  array.sort! {|a,b| a.length<=>b.length}
+  return array
+end
+
 def swap_elements(array)
   temp=array[1]
   array[1]=array[2]
